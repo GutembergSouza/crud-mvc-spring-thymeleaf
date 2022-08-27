2093f1bbe4b9ff29d8105d6bd569ee40f31bef44
GutembergSouza <gutembergsiqueira96@gmail.com>
2022-08-27 12:27:00 -0300

    ADICIONANDO FUNCIONALIDADE DE ALTERAÇÃO

---
+++ src/main/java/com/Gu/cad/CadCli/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.Gu.cad.CadCli.controller;
+
+
+import com.Gu.cad.CadCli.model.Login;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String getLogin(){
+        return "loginIndex";
+    }
+
+    @PostMapping("/")
+    public String login(@ModelAttribute(name="login") Login login, Model model){
+        String email = login.getEmail();
+        String senha = login.getSenha();
+
+        if ("teste@teste.com".equals(email) && "123456".equals(senha) ){
+            return "redirect:/clientes";
+        }
+        model.addAttribute("message", "Dados invalidos");
+        return "loginIndex";
+
+    }
+
+
+
+}
